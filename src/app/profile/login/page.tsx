489026bcab8d90439390b90975bdba5ab986e135
qwerttyuiiop <sherwynn_angeles@dlsu.edu.ph>
2023-10-20 23:11:17 +0800

    implement login page

--- src/app/profile/login/page.tsx
+++ src/app/profile/login/page.tsx
@@ -1,15 +1,41 @@
 "use client"
-import styles from "./login.module.css";
+import styles from "./page.module.css";
 import React from "react";
 import { useSession } from "next-auth/react";
-import { CardPage, Card, Title } from "@/components/CardPage/CardPage";
+import { 
+	CardPage, 
+	Card, 
+	Title,
+	Input,
+	Password,
+	BigButton,
+	Separator,
+	CardRow,
+	SmallButton
+} from "@/components/CardPage/CardPage";
+import { FcGoogle } from "react-icons/fc"
 
 
 export default function Page() {
   return (
 	<CardPage>
 	  <Card>
-		<Title> Login Page </Title>
+		<Title> Login </Title>
+		<Input placeholder="Username" id="username"/>
+		<Password placeholder="Password" id="password"/>
+		<BigButton> Login </BigButton>
+		<Separator text="OR"/>
+		<BigButton>
+		  <CardRow>
+		  	<FcGoogle className={styles.google_icon}/>
+		  	<div className={styles.google_text}> Sign-in with Google </div>
+		  </CardRow>
+		</BigButton>
+		<CardRow>
+		  <div className={styles.no_account}> Don&apos;t have an account? </div>
+		  <SmallButton> Sign-up </SmallButton>
+		</CardRow>
+		<div className={styles.forgot_password}> Forgot your password? </div>
 	  </Card>
 	</CardPage>
   );
--- src/components/CardPage/CardPage.tsx
+++ src/components/CardPage/CardPage.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import styles from "./card.module.css";
+import { BsFillEyeFill, BsEyeSlashFill } from "react-icons/bs";
 
 interface Children {
 	children: ReactNode;
@@ -13,13 +14,77 @@ function wrapDiv(className: string): React.FC<Children> {
 	return div;
 }
   
-const CardPage = wrapDiv(styles.main_container);
+const CardPage = wrapDiv(`${styles.main_container} ${styles.background}`);
 const Card = wrapDiv(styles.card);
 const Title = wrapDiv(styles.title);
+
+interface InputProps {
+	placeholder: string;
+	id: string;
+}
+const Input: React.FC<InputProps & { type?: string }> = ({ placeholder, id, type }) => (
+  <input type={type || "text"} placeholder={placeholder} id={id} className={styles.input}/>
+);
+const Password: React.FC<InputProps> = ({ placeholder, id }) => {
+	const [showPassword, setShowPassword] = React.useState(false);
+	return (
+	  <div className={styles.relative}>
+		<input 
+		  type={showPassword ? "text" : "password"}
+		  placeholder={placeholder} 
+		  id={id} 
+		  className={`${styles.input} ${styles.password}`}
+		  />
+		{showPassword ? (
+		  <BsEyeSlashFill
+			className={styles.password_icon} 
+			onClick={() => setShowPassword(!showPassword)}
+			/>
+		) : (
+		  <BsFillEyeFill
+			className={styles.password_icon}
+			onClick={() => setShowPassword(!showPassword)}
+			/>
+		)}
+	  </div>
+	);
+};
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	children: ReactNode;
+}
+const BigButton: React.FC<ButtonProps> = ({ children, ...props }) => (
+  <button className={styles.big_button} {...props}>
+	{children}
+  </button>
+);
+const SmallButton: React.FC<ButtonProps> = ({ children, ...props }) => (
+  <button className={styles.small_button} {...props}>
+	{children}
+  </button>
+);
+
+const Separator: React.FC<{ text: string }> = ({ text }) => {
+	return (
+	  <div className={styles.separator}>
+		<hr className={styles.separator_line}/>
+		<span className={styles.separator_text}> {text} </span>
+		<hr className={styles.separator_line}/>
+	  </div>
+	);
+
+}
+const CardRow = wrapDiv(styles.card_row);
 
 
 export { 
 	CardPage,
 	Card,
 	Title,
+	Input,
+	Password,
+	BigButton,
+	Separator,
+	CardRow,
+	SmallButton
 };
