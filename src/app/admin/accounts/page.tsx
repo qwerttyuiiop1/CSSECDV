d7009ebe0841d9830f451b5dbc9faf40cd71384d
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2023-10-23 19:03:58 +0800

    initialize accounts folder

---
+++ src/app/admin/accounts/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function page() {
+    return(
+        <div>
+            <h1>
+                
+            </h1>
+        </div>
+    );
+}
