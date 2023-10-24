8b5fa2395e53b09bb33282905cc5d3cadc8d984f
howard-rose <philip.anthon@gmail.com>
2023-10-24 10:42:57 +0800

    button clicking, separated section components

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -24,82 +24,88 @@ export default function Wallet({
 }) {
     return (
         <>
-            <section className={styles.section}>
-                <div><h1>My Balance</h1> <img src="/icons/eyes.svg" alt="Eye Toggle" /></div>
-                <div className={styles.balance_subsection}>
-                    <div className={`${styles.points_container} ${styles.content_block} ${styles.block_dark}`}>
-                        <IconTextWrapper src="/icons/gift.svg" alt="Reward Points Icon" text="Reward Points" />
-                        <div>{balance.points}</div>
-                        <IconTextWrapper className={`${styles.button} ${styles.points_add}`} src="/icons/plus.svg" alt="Add Points Icon" text="Add Points" />
-                    </div>
-                    <div className={`${styles.token_container} ${styles.content_block} ${styles.block_light}`}>
-                        <IconTextWrapper src="/icons/token.svg" alt="Token Icon" text="Token" />
-                        <div>{balance.token}</div>
-                    </div>
-                </div>
-            </section>
-            <section className={styles.section}>
-                <div><h1>My Profile</h1></div>
-                <div className={styles.profile_subsection}>
-                    <div className={`${styles.profile_container} ${styles.content_block} ${styles.block_dark}`}>
-                        <div className={styles.profile_table_container}>Profile Table</div>
-                        <div className={`${styles.profile_cog_container} ${styles.button} ${styles.profile_edit}`}><img src="/icons/gear.svg" alt="Edit Profile"/></div>
-                    </div>
-                    <div className={`${styles.wallet_container} ${styles.content_block} ${styles.block_light}`}>
-                        <IconTextWrapper src="/icons/WalletConnect-Logo1.svg" alt="Wallet Connect Logo" text="Wallet" />
-                        <div>No Wallet Connected</div>
-                        <IconTextWrapper className={`${styles.button} ${styles.wallet_connect}`} src="/icons/link.svg" alt="Link Icon" text="Connect Wallet" />
-                    </div>
-                </div>
-            </section>
-            <section className={styles.section}>
-                <div><h1>My Transactions</h1></div>
-                <div className={styles.transactions_subsection}>
-                    <div className={styles.transactions_page_container}>
-                        <div className={styles.transactions_amount_display}>
-                            Showing 1-15 of 213
-                        </div>
-                        <div className={styles.transactions_page_selector}>
-                            Page 1
-                        </div>
-                    </div>
-                    <div className={styles.transactions_table_container}>
-                        Table Contents
-                    </div>
-                </div>
-            </section>
+            <BalanceSection balance={balance} />
+            <ProfileSection profile={profile} />
+            <TransactionsSection transactions={transactions} />
         </>
     );
 }
 
-// function HeaderSection({ children, className="", header, afterHeader=null }) {
-//     return (
-//         <section className={styles.section}>
-//             <div><h1>{header}</h1>{afterHeader}</div>
-//             <div className={className}>
-//                 {children}
-//             </div>
-//         </section>
-//     );
-// }
-
-function IconTextWrapper({ className="", src, alt, text }) {
+function HeaderSection({ children, className="", header, afterHeader=null }) {
     return (
-        <div className={`${styles.icon_text_wrapper} ${className}`}>
+        <section className={styles.section}>
+            <div><h1>{header}</h1> {afterHeader}</div>
+            <div className={className}>
+                {children}
+            </div>
+        </section>
+    );
+}
+
+function IconTextWrapper({ className="", src, alt, text="" }) {
+    return (
+        <div className={`${text && styles.icon_text_wrapper} ${className}`}>
             <img src={src} alt={alt} /> 
-            <span>{text}</span>
+            {text && (<span>{text}</span>)}
         </div>
     );
 }
 
-function BalanceSection() {
-
+function IconTextButton({ className="", src, alt, text, onClick }) {
+    return (
+        <div onClick={onClick}>
+            <IconTextWrapper className={`${className} ${styles.button}`} src={src} alt={alt} text={text} />
+        </div>
+    );
 }
 
-function ProfileSection() {
-
+function BalanceSection({ balance }) {
+    return (
+        <HeaderSection className={styles.balance_subsection} header="My Balance" afterHeader={(<img src="/icons/eyes.svg" alt="Eye Toggle" />)}>
+            <div className={`${styles.points_container} ${styles.content_block} ${styles.block_dark}`}>
+                <IconTextWrapper src="/icons/gift.svg" alt="Reward Points Icon" text="Reward Points" />
+                <div>{balance.points}</div>
+                <IconTextButton onClick={() => console.log("Clicked")} className={styles.points_add} src="/icons/plus.svg" alt="Add Points Icon" text="Add Points" />
+            </div>
+            <div className={`${styles.token_container} ${styles.content_block} ${styles.block_light}`}>
+                <IconTextWrapper src="/icons/token.svg" alt="Token Icon" text="Token" />
+                <div>{balance.token}</div>
+            </div>
+        </HeaderSection>
+    );
 }
 
-function TransactionsSection() {
+function ProfileSection({ profile }) {
+    return (
+        <HeaderSection className={styles.profile_subsection} header="My Profile">
+            <div className={`${styles.profile_container} ${styles.content_block} ${styles.block_dark}`}>
+                <div className={styles.profile_table_container}>Profile Table</div>
+                {/* <HeaderSection>div className={`${styles.profile_cog_container} ${styles.button} ${styles.profile_edit}`}><img src="/icons/gear.svg" alt="Edit Profile"/></div> */}
+                <IconTextButton onClick={() => console.log("Clicked")} className={`${styles.profile_edit} ${styles.profile_cog_container}`} src="/icons/gear.svg" alt="Edit Profile" text="" />
+            </div>
+            <div className={`${styles.wallet_container} ${styles.content_block} ${styles.block_light}`}>
+                <IconTextWrapper src="/icons/WalletConnect-Logo1.svg" alt="Wallet Connect Logo" text="Wallet" />
+                <div>No Wallet Connected</div>
+                <IconTextButton onClick={() => console.log("Clicked")} className={styles.wallet_connect} src="/icons/link.svg" alt="Link Icon" text="Connect Wallet" />
+            </div>
+        </HeaderSection>
+    );
+}
 
+function TransactionsSection({ transactions }) {
+    return (
+        <HeaderSection className={styles.transactions_subsection} header="My Transactions">
+            <div className={styles.transactions_page_container}>
+                <div className={styles.transactions_amount_display}>
+                    Showing 1-15 of 213
+                </div>
+                <div className={styles.transactions_page_selector}>
+                    Page 1
+                </div>
+            </div>
+            <div className={styles.transactions_table_container}>
+                Table Contents
+            </div>
+        </HeaderSection>
+    );
 }
