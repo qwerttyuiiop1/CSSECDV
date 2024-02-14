6695b1ebebffa9d4028ce3fb9433bd971f9919ed
nidorb <gregory_charles_tiong@dlsu.edu.ph>
2024-02-14 18:04:46 +0800

    Added Frontend Validation for Pfp

--- src/app/profile/signup/PfpCard.tsx
+++ src/app/profile/signup/PfpCard.tsx
@@ -1,67 +1,84 @@
 "use client";
 import styles from "../login.module.css";
 import React from "react";
-import { 
-	Card, 
-	Title,
-	CardRow,
-	SmallButton,
-	SideButton,
-	FileInput
-} from "@/components/CardPage/CardPage";
-import { LuArrowLeft } from "react-icons/lu"
-import { useForm } from "react-hook-form";
 import {
-	FormContainer,
-	useFormError
-} from "@/components/Providers/Forms";
+  Card,
+  Title,
+  CardRow,
+  SmallButton,
+  SideButton,
+  FileInput,
+} from "@/components/CardPage/CardPage";
+import { LuArrowLeft } from "react-icons/lu";
+import { useForm } from "react-hook-form";
+import { FormContainer, useFormError } from "@/components/Providers/Forms";
 import ReCaptcha from "react-google-recaptcha";
 
 export interface PfpCardOutput {
-	pfp: FileList;
-	recaptcha: string;
+  pfp: FileList;
+  recaptcha: string;
 }
 export interface PfpCardProps {
-	onBack: (output: PfpCardOutput) => void;
-	onSubmit: (output: PfpCardOutput) => void;
-	data: PfpCardOutput | null;
+  onBack: (output: PfpCardOutput) => void;
+  onSubmit: (output: PfpCardOutput) => void;
+  data: PfpCardOutput | null;
 }
 
 export default function UserCard({ onSubmit, onBack, data }: PfpCardProps) {
-	const form = useForm({ defaultValues: data || undefined });
-	const [ recaptcha, setRecaptcha ] = React.useState<string | null>(null);
-	const recaptchaRef = React.useRef<ReCaptcha>(null);
+  const form = useForm({ defaultValues: data || undefined });
+  const [recaptcha, setRecaptcha] = React.useState<string | null>(null);
+  const recaptchaRef = React.useRef<ReCaptcha>(null);
 
-	const handleSubmit = form.handleSubmit((v) => {
-		if (!recaptcha) 
-			form.setError('recaptcha', { type: 'manual', message: 'recaptcha is required' });
-		else
-			onSubmit({ ...v, recaptcha });
-	})
-	const handleBack = () => onBack(form.getValues());
-	useFormError(form);
-	return (
-		<FormContainer form={form}>
-		<Card>
-			<Title>Sign-up</Title>
-			<FileInput image id="pfp" accept="image/*" options={{ required: "Please upload a profile picture" }}/>
-			<ReCaptcha
-				ref={recaptchaRef}
-				sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ''}
-				onChange={setRecaptcha}
-				/>
-			<CardRow className={styles.width}>
-				<SmallButton onClick={handleBack}>
-					<CardRow>
-						<LuArrowLeft className={styles.arrow}/>
-						<span className={styles.back_text}>Back</span>
-					</CardRow>
-				</SmallButton>
-				<SideButton color="blue" onClick={handleSubmit} side="right">
-					Create Account
-				</SideButton>
-			</CardRow>
-		</Card>
-		</FormContainer>
-	);
+  const handleSubmit = form.handleSubmit((v) => {
+    if (!recaptcha)
+      form.setError("recaptcha", {
+        type: "manual",
+        message: "recaptcha is required",
+      });
+    else onSubmit({ ...v, recaptcha });
+  });
+  const handleBack = () => onBack(form.getValues());
+  useFormError(form);
+  return (
+    <FormContainer form={form}>
+      <Card>
+        <Title>Sign-up</Title>
+        <FileInput
+          image
+          id="pfp"
+          accept="image/jpeg, image/png, image/webp"
+          options={{
+            required: "Please upload a profile picture",
+            validate: {
+              isImage: (file) =>
+                file[0]?.type === "image/jpeg" ||
+                file[0]?.type === "image/png" ||
+                file[0]?.type === "image/webp" ||
+                "Please upload an image file",
+              maxSize: (file) =>
+                file[0]?.size <= 10 * 1024 * 1024 ||
+                "File should have maximum size of 10MB",
+            },
+          }}
+        />
+
+        <ReCaptcha
+          ref={recaptchaRef}
+          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
+          onChange={setRecaptcha}
+        />
+        <CardRow className={styles.width}>
+          <SmallButton onClick={handleBack}>
+            <CardRow>
+              <LuArrowLeft className={styles.arrow} />
+              <span className={styles.back_text}>Back</span>
+            </CardRow>
+          </SmallButton>
+          <SideButton color="blue" onClick={handleSubmit} side="right">
+            Create Account
+          </SideButton>
+        </CardRow>
+      </Card>
+    </FormContainer>
+  );
 }
