8b4c56e9400c434aa468b4170173a87490181023
nidorb <gregory_charles_tiong@dlsu.edu.ph>
2024-03-20 11:34:12 +0800

    Update SessionTimeout.tsx

--- src/components/SessionTimeout/SessionTimeout.tsx
+++ src/components/SessionTimeout/SessionTimeout.tsx
@@ -2,23 +2,27 @@
 import React, { useState, useEffect } from "react";
 import SessionTimeoutModal from "./SessionTimeoutModal";
 import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 let timeoutId: NodeJS.Timeout;
 
 const YourComponent: React.FC = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   const user = session?.user;
 
-  const resetTime = () => {
+  const resetTime = async () => {
     clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
+    timeoutId = setTimeout(async () => {
       console.log("Session timeout");
       if (user != null) {
         console.log("Logging out user");
-        signOut();
+        await signOut({ redirect: false });
+        router.push("/profile/login");
       }
-    }, 1 * 1 * 1000); // 1 minute || time in milliseconds
+    }, 1 * 60 * 1000); // 1 minute || time in milliseconds
   };
+  
 
   const handleUserActivity = () => {
     resetTime();
