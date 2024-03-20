2d5f2d47afad94122bcad0d0cf85e71b650b1776
nidorb <gregory_charles_tiong@dlsu.edu.ph>
2024-03-20 23:06:52 +0800

    Update validate.ts

--- src/app/api/shop/validate.ts
+++ src/app/api/shop/validate.ts
@@ -12,32 +12,6 @@ export async function validateUpload(data: FormData) {
     if (file.size > 50 * 1024 * 1024) {
       return "File size exceeds the maximum allowed limit (50MB).";
     }
-  
-    const csvData = await file.text();
-
-    const rows = csvData.split(/\r?\n/);
-
-    const nonEmptyRows = rows.filter(row => row.trim() !== '');
-
-    for (const row of nonEmptyRows) {
-        const values = row.trim().split(/\s*,\s*/);
-
-        if (values.length !== 3) {
-            return "All rows must contain exactly 3 values.";
-        }
-
-        if (values[0] === '') {
-            return "Shop cannot be blank";
-        }
-
-        if (values[1] === '') {
-            return "Product cannot be blank";
-        }
-
-        if (values[2] === '') {
-            return "Code cannot be blank";
-        }
-    }
 
     return true;
 }
