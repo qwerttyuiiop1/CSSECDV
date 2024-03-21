9b0c62f4c5c9144709f9dec502251fd2003bd303
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-03-21 18:20:36 +0800

    update report access

--- src/app/api/profile/report/route.ts
+++ src/app/api/profile/report/route.ts
@@ -1,18 +1,8 @@
 import { withUser } from '@/lib/session/withUser'
 import { Report, mapReport, reportSelection } from '@/lib/types/Transaction'
 import prisma from '@prisma'
+import { UserRole } from '@prisma/client';
 import { NextResponse } from 'next/server'
-
-export const GET = withUser(async (req) => {
-	const res = await prisma.productReport.findMany({
-		where: {
-			userEmail: req.user.email
-		},
-		...reportSelection
-	})
-	const reports: Report[] = res.map(mapReport)
-	return NextResponse.json({ reports })
-})
 
 export const POST = withUser(async (req) => {
 	try {
@@ -21,7 +11,16 @@ export const POST = withUser(async (req) => {
 			return NextResponse.json({ error: 'Invalid comment' }, { status: 400 })
 		if (typeof code !== 'string' || typeof productId !== 'number')
 			return NextResponse.json({ error: 'Invalid code or productId' }, { status: 400 })
-
+		const ownerEmail = await prisma.codeH.findUnique({
+			where: { 
+				code_productId: { productId, code }
+			},
+			select: { transaction: { select: { userEmail: true } } }
+		})
+		if (!ownerEmail)
+			return NextResponse.json({ error: 'Code not found' }, { status: 404 })
+		if (ownerEmail.transaction.userEmail !== req.user.email && req.user.role !== UserRole.ADMIN)
+			return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
 		const res = await prisma.productReport.create({
 			data: { 
 				comment: comment,
